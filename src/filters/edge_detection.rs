a2e6bcafc72535ff4b1c44077a148b5234c29b68
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-01-07 09:35:03 -0300

    feat: impl filter for EdgeDetection

--- src/filters/edge_detection.rs
+++ src/filters/edge_detection.rs
@@ -3,10 +3,71 @@ use super::{Filter, FilterError};
 pub struct EdgeDetection;
 
 //# # #    //# # #   //# # #
-const KERNEL_TABLE: [i8; 9] = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
+const KERNEL_TABLE: [i16; 9] = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
 
 impl Filter for EdgeDetection {
     fn apply(&self, image: &mut impl crate::images::Image) -> Result<(), FilterError> {
+        let size = image.get_pixels().len();
+        let widht = image.widht();
+
+        let pixels = image
+            .get_pixels()
+            .iter()
+            .map(|c| c.grayscale())
+            .collect::<Vec<_>>();
+
+        for index in 0..size {
+            let up_row = index.saturating_sub(widht) / widht;
+            let mid_row = index / widht;
+            let down_row = (index + widht).clamp(index, size - 1) / widht;
+
+            let pxs = [
+                clamp_index_in_row(widht, up_row, index.saturating_sub(widht + 1)), // 0
+                clamp_index_in_row(widht, up_row, index.saturating_sub(widht)),     // 1
+                clamp_index_in_row(widht, up_row, index.saturating_sub(widht - 1)), // 2
+                // # # #
+                clamp_index_in_row(widht, mid_row, index.saturating_sub(1)), // 3
+                clamp_index_in_row(widht, mid_row, index),                   // 4
+                clamp_index_in_row(widht, mid_row, index + 1),               // 5
+                // # # #
+                clamp_index_in_row(widht, down_row, index + widht - 1), // 6
+                clamp_index_in_row(widht, down_row, index + widht),     // 7
+                clamp_index_in_row(widht, down_row, index + widht + 1), // 8
+            ];
+
+            let gx = (pixels[pxs[0]].red() as i16 * KERNEL_TABLE[0]
+                + pixels[pxs[3]].red() as i16 * KERNEL_TABLE[3]
+                + pixels[pxs[6]].red() as i16 * KERNEL_TABLE[6])
+                + (pixels[pxs[2]].red() as i16 * KERNEL_TABLE[2]
+                    + pixels[pxs[5]].red() as i16 * KERNEL_TABLE[5]
+                    + pixels[pxs[8]].red() as i16 * KERNEL_TABLE[8]);
+
+            let gy = (pixels[pxs[0]].red() as i16 * KERNEL_TABLE[0]
+                + pixels[pxs[2]].red() as i16 * KERNEL_TABLE[3]
+                + pixels[pxs[3]].red() as i16 * KERNEL_TABLE[6])
+                + (pixels[pxs[6]].red() as i16 * KERNEL_TABLE[2]
+                    + pixels[pxs[7]].red() as i16 * KERNEL_TABLE[5]
+                    + pixels[pxs[8]].red() as i16 * KERNEL_TABLE[8]);
+
+            let magnitude = (gx.abs() + gy.abs()).clamp(0, 255) as u8;
+
+            let color = &mut image.pixels()[index];
+            color.set_value(magnitude);
+
+            /*if magnitude > 80 {
+                color.set_value(255);
+            } else {
+                color.set_value(0)
+            };*/
+        }
+
         Ok(())
     }
+}
+
+fn clamp_index_in_row(row_size: usize, row_index: usize, index: usize) -> usize {
+    let row_start = row_size * row_index;
+    let row_end = row_start + row_size - 1;
+
+    index.clamp(row_start, row_end)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use crate::{
-    filters::{
-        box_blur::BoxBlur, flip_h::FlipH, flip_v::FlipV, grayscale::GrayScale, negative::Negative,
-    },
+    filters::*,
     images::{Image, bitmap::Bitmap},
 };
 
@@ -9,38 +7,8 @@ mod filters;
 mod images;
 
 fn main() {
-    let mut bitmap = Bitmap::open("./images/tower.bmp").unwrap();
+    let mut bitmap = Bitmap::open("./images/stadium.bmp").unwrap();
 
-    /*
-    let _ = bitmap.filter(FlipH::full());
-    let _ = bitmap.save("./images/clone1.bmp").unwrap();
-
-    let mut bitmap = Bitmap::open("./images/tower.bmp").unwrap();
-    let _ = bitmap.filter(FlipH::rect((0, 0), (600, 200)));
-    let _ = bitmap.save("./images/clone2.bmp").unwrap();
-
-    let mut bitmap = Bitmap::open("./images/negative.bmp").unwrap();
-    let _ = bitmap.filter(Negative);
-    let _ = bitmap.save("./images/negative2.bmp").unwrap();
-    */
-
-    let _ = bitmap.filter(FlipV::full());
-    let _ = bitmap.save("./images/clone1.bmp").unwrap();
-
-    let mut bitmap = Bitmap::open("./images/tower.bmp").unwrap();
-    let _ = bitmap.filter(FlipV::rect((0, 0), (600, 200)));
-    let _ = bitmap.filter(Negative);
-    let _ = bitmap.save("./images/clone2.bmp").unwrap();
-
-    let mut bitmap = Bitmap::open("./images/tower.bmp").unwrap();
-    let _ = bitmap.filter(GrayScale);
-    let _ = bitmap.save("./images/gray.bmp").unwrap();
-
-    let mut bitmap = Bitmap::open("./images/tower.bmp").unwrap();
-    let _ = bitmap.filter(BoxBlur(1));
-    let _ = bitmap.save("./images/blur1.bmp").unwrap();
-
-    let mut bitmap = Bitmap::open("./images/tower.bmp").unwrap();
-    let _ = bitmap.filter(BoxBlur(10));
-    let _ = bitmap.save("./images/blur2.bmp").unwrap();
+    let _ = bitmap.filter(edge_detection::EdgeDetection);
+    let _ = bitmap.save("./images/edges.bmp").unwrap();
 }
